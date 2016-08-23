ae7ca3d9d4e27b55eb31c8519d94074e14f38373
zzjjhh <Luxi@Luxi-PC>
2016-08-23 15:49:46 +0800

    dgd

---
+++ src/com/sg/test/Test.java
@@ -0,0 +1,12 @@
+/**
+ *2016年8月23日
+ *下午3:49:21
+ *http://www.itzoo.net
+ */
+package com.sg.test;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println(12315);
+}
+}
